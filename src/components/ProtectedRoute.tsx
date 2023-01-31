a13d2ea3850c6333ad1a91ead8b1f2756702dd62
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-01-30 22:01:30 -0300

    actualizando rutas protegidas por privilegio

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,22 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store/auth";
 
 interface Props {
   isAllowed: Boolean;
+  allowedRoles: number[];
 }
 
-export const ProtectedRoute = ({ isAllowed }: Props) => {
+
+export const ProtectedRoute = ({ isAllowed, allowedRoles }: Props) => {
+  const roles: any = useAuthStore((state) => state.privilegio);
   if (!isAllowed) {
     return <Navigate to={"/login"} />;
+    
   }
+
+  if(!allowedRoles?.includes(roles)){
+    return <Navigate to={"/NotPermissionPage"} />;
+  }
+
   return <Outlet />;
 };
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -11,22 +11,51 @@ import { useAuthStore } from "../store/auth";
 
 function AppRoutes() {
   const isAuth = useAuthStore((state) => state.isAuth);
+  const ROL ={
+    admin: 1,
+    coordinador: 2,
+    despachador: 3,
+    lector: 4
+  }
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route element={<PublicRoute />}> */}
         <Route path="/" index element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
-        {/* </Route> */}
-        <Route element={<ProtectedRoute isAllowed={isAuth} />}>
+        
+          {/* Rutas Privadas */}
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} allowedRoles={[ROL.admin, ROL.coordinador, ROL.despachador, ROL.lector]} />}
+        >
           <Route path="/home" index element={<HomePage />} />
+        </Route>
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} allowedRoles={[ROL.admin, ROL.coordinador,ROL.lector]} />}
+        >
           <Route path="/usuarios" element={<UsuariosPage />} />
+        </Route>
+
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} allowedRoles={[ROL.admin, ROL.coordinador,ROL.lector]} />}
+        >
           <Route path="/vehiculos" element={<VehiculosPage />} />
+        </Route>
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} allowedRoles={[ROL.admin, ROL.coordinador,ROL.lector]} />}
+        >
           <Route path="/sucursales" element={<SucursalesPage />} />
+        </Route>
+
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} allowedRoles={[ROL.admin, ROL.coordinador,ROL.lector]} />}
+        >
           <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+        <Route
+          element={<ProtectedRoute isAllowed={isAuth} allowedRoles={[ROL.admin, ROL.coordinador, ROL.despachador, ROL.lector]} />}
+        >
           <Route path="*" element={<NotFoundPage />} />
         </Route>
-        
       </Routes>
     </BrowserRouter>
   );
