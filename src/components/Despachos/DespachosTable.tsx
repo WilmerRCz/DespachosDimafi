07bbfd51767854f231eb1e30d32bbdf1ac07bcc4
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-02-14 21:32:09 -0300

    probando boton de visualizar información

--- src/components/Despachos/DespachosTable.tsx
+++ src/components/Despachos/DespachosTable.tsx
@@ -1,8 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
-import { Table } from 'antd';
-import { getDespachos } from '../../api/resDespachos';
-import { Despachos } from '../../interface/Despachos';
-import { columns } from './columsDespachosTable';
+import { useQuery } from "@tanstack/react-query";
+import { Drawer, Table } from "antd";
+import { getDespachos } from "../../api/resDespachos";
+import { Despachos } from "../../interface/Despachos";
+import { FiEye, FiEdit3, FiTrash2 } from "react-icons/fi";
+import { AlignType } from "rc-table/lib/interface";
+import { useState } from "react";
 
 function DespachosTable() {
   const { data, isLoading, isError } = useQuery({
@@ -10,28 +12,105 @@ function DespachosTable() {
     queryFn: getDespachos,
   });
 
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   if (isLoading) return <div>Cargando...</div>;
   else if (isError) return <div>Error: desde react query</div>;
 
-  const fileData = 
-    data.slice(0).reverse().map((despacho: Despachos) => (
-      {
+
+  const columns = [
+    {
+      title: "Nro",
+      dataIndex: "nro",
+      key: "nro",
+      align: "center" as AlignType,
+    },
+    {
+      title: "Rut",
+      dataIndex: "rut",
+      key: "rut",
+      align: "center" as AlignType,
+    },
+    {
+      title: "Dirección",
+      dataIndex: "direccion",
+      key: "direccion",
+      align: "center" as AlignType,
+    },
+    {
+      title: "Despachador",
+      dataIndex: "despachador",
+      key: "despachador",
+      align: "center" as AlignType,
+    },
+    {
+      title: "Estado",
+      dataIndex: "estado",
+      key: "estado",
+      align: "center" as AlignType,
+    },
+    {
+      title: "Acciones",
+      dataIndex: "acciones",
+      key: "acciones",
+      align: "center" as AlignType,
+      render: (text:any, record: any, index:any) => {
+        return (
+          <div className="flex gap-4 justify-center">
+            <button>
+              <FiEye size={19} onClick={showDrawer} />
+            </button>
+            <Drawer
+              title="Despacho"
+              placement={"right"}
+              width={400}
+              onClose={onClose}
+              open={open}
+            >
+              <div>
+                <p>{JSON.stringify(data[record.key])}</p>
+              </div>
+            </Drawer>
+            <button>
+              <FiEdit3 size={19} color={"#FFC300"} />
+            </button>
+
+            <button>
+              <FiTrash2 size={19} color={"red"} />
+            </button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const fileData = data
+    .slice(0)
+    .reverse()
+    .map((despacho: Despachos) => ({
       key: despacho.id_despacho,
       nro: despacho.id_despacho,
       rut: despacho.rut_cliente_despacho,
       direccion: `${despacho.direccion_calle_cliente}, ${despacho.nro_calle_cliente} - ${despacho.nombre_comuna}`,
       despachador: despacho.usuario_despachador,
-      estado: despacho.nombre_estado
-    }
-    ))
-  
+      estado: despacho.nombre_estado,
+    }));
+
+
 
   return (
-    <div className='mt-2'>
+    <div className="mt-2">
       <Table columns={columns} dataSource={fileData} />
     </div>
-  )
+  );
 }
 
-export default DespachosTable
+export default DespachosTable;
