d0037f86c8edbdce3bf7853ec26429e67c78a509
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-10-07 19:03:08 -0300

    modificación de pagina 404 not found y sin permisos suficientes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,18 @@
+import Navbar from '../components/Navbar/Navbar'
 import TitlePage from "../components/common/TitlePage";
 
 function NotFoundPage() {
-  return <TitlePage title="Página no encontrada" />;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-grow flex justify-center items-center">
+        <div className="bg-white p-8 rounded-lg shadow-md">
+          <h1 className="text-slate-700 text-2xl sm:text-6xl font-semibold "><span className='text-4xl sm:text-7xl'>404</span> - Página no encontrada</h1>
+          <p className="text-slate-700 sm:text-xl mt-4">Lo sentimos, la página que estás buscando no existe.</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NotFoundPage;
--- src/pages/NotPermissionPage.tsx
+++ src/pages/NotPermissionPage.tsx
@@ -3,12 +3,15 @@ import TitlePage from "../components/common/TitlePage";
 
 function NotPermissionPage() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="sm:container xl:px-16 mx-auto">
-        <TitlePage title="No tienes Permisos" />
+      <div className="flex-grow flex justify-center items-center">
+        <div className="bg-white p-8 rounded-lg shadow-md">
+          <h1 className="text-slate-700 text-2xl sm:text-6xl font-semibold ">No tiene acceso</h1>
+          <p className="text-slate-700 sm:text-xl mt-4">Lo sentimos, no posee los permisos suficientes para acceder.</p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
